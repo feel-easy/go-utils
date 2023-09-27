5a47d9a4cdb3d0f632d7d278ca0161985c60b69e
wangshenglei <wangshenglei@zhuanzhuan.com>
2023-09-27 15:45:06 +0800

    translate

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/feel-easy/go-utils
 go 1.16
 
 require (
-	github.com/hnmaonanbei/go-youdao-fanyi v0.0.0-20201126033016-44ac799c4cd0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/antchfx/htmlquery v1.3.0
+	github.com/json-iterator/go v1.1.12
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
 )
--- translate/main.go
+++ translate/main.go
@@ -2,46 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"os/exec"
+	"strings"
 
-	ydfanyi "github.com/hnmaonanbei/go-youdao-fanyi"
+	"github.com/antchfx/htmlquery"
 )
 
+const URL = "https://dict.youdao.com/w/eng/%s/#keyfrom=dict2.index"
+
+func translate(words string) string {
+	words = strings.ReplaceAll(words, "/", "／")
+	url := fmt.Sprintf(URL, words)
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	doc, err := htmlquery.Parse(strings.NewReader(string(content)))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	result := htmlquery.FindOne(doc, "//div[@id='results-contents']//ul")
+	// content = []byte(htmlquery.OutputHTML(result, true))
+	content = []byte(htmlquery.InnerText(result))
+	return string(content)
+}
+
 func main() {
-	keys := make(map[int]bool)
-
-	for k, _ := range os.Args {
-		keys[k] = true
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a word to translate.")
+		os.Exit(1)
 	}
 
-	if _, ok := keys[1]; !ok {
-		fmt.Println("USEAGE: ")
-		fmt.Println("\t- $ yd 词/句 [是否读出来]")
-		fmt.Println("\t- $ yd hello 1 /* 试一下 */")
-		return
-	}
-	opts := ydfanyi.NewOptions("", "", "")
-	// opts.proxy = ""
-	//opts.From = ydfanyi.EN
-	//opts.To = ydfanyi.ZH
-	input := os.Args[1]
-	res, _ := ydfanyi.Do(input, opts)
-	// 翻译结果
-	fmt.Println(res.String())
-	// 详细信息
-	fmt.Println(res.SmartResult.Entries)
-	// out
-
-	//	你好
-	//	[ int. 喂；哈罗，你好，您好（表示问候， 惊奇或唤起注意时的用语）
-	//	n. （Hello）（法）埃洛（人名）
-	//]
-	if _, ok := keys[2]; ok {
-		cmd := exec.Command("say", input)
-		if err := cmd.Run(); err != nil {
-			fmt.Println("出错啦：不好意思，我不知道怎么读")
-		}
-	}
-
+	result := translate(os.Args[1])
+	fmt.Println(result)
 }
